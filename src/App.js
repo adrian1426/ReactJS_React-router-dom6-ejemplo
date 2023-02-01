78c0b86f6fcdb3e36c4e26d3f6454390acec1788
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-02-01 12:09:24 -0600

    Configurando nuestras primeras rutas y ruta not found

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Home</h1>,
+    errorElement: <h2>No se encontró la página amigo.</h2>
+  },
+  {
+    path: '/about',
+    element: <h1>About</h1>
+  },
+  {
+    path: '/contact',
+    element: <h1>Contact</h1>
+  }
+]);
+
 function App() {
   return (
     <div>
       <h1> Mi app</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
