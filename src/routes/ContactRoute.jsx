e32d6978fbd63015b2197bc8c03410478064f1d2
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-02-05 21:15:42 -0600

    mostrando en pantalla info de contactos de manera dinamica

--- src/routes/ContactRoute.jsx
+++ src/routes/ContactRoute.jsx
@@ -1,11 +1,16 @@
+import { useMemo } from "react";
 import { useParams } from "react-router-dom";
+import { getContactById } from "../data/items";
 
 const ContactRoute = () => {
   const params = useParams();
 
+  const contact = useMemo(() => getContactById(params.contactId), [params.contactId]);
+
   return (
     <div>
-      Contact {params.contactId}
+      <label>name: {contact.name}</label>
+      <label>phone: {contact.telephone}</label>
     </div>
   );
 };
